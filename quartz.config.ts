c9893ad7d7201cefed4f0b27cd5a8df7ec82e70c
Hermann Wall <wall@MiniM4.fritz.box>
2025-11-28 01:24:08 +0100

    Quartz sync: Nov 28, 2025, 1:24 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://Hermann-85.github.io/hwall",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
